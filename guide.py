a0ea8fe5ba2890dfd4672157eeced113e939421e
Joan Salvatella <joan@bookline.io>
2024-11-09 05:50:58 +0100

    test

--- guide.py
+++ guide.py
@@ -1,22 +1,22 @@
 path_map = {
     1: {
-        "x_path": '//*[@id="hTContainer"]/div/div[1]/div[2]/nav/div/div/ul/li[5]/button',
+        "x_path": '/html/body/div/header/div/div/div/div[2]/nav/div/div/ul/li[5]/button',
         "text": "Situacions de vida"
     },
     2: {
-        "x_path": '//*[@id="hTContainer"]/div/div[1]/div[2]/nav/div/div/ul/li[5]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/ul/li[6]/button',
+        "x_path": '/html/body/div/header/div/div/div/div[2]/nav/div/div/ul/li[5]/div/div[2]/div/div/div/div[2]/div/div/div/ul/li[6]/button',
         "text": "Mobilitat"
     },
     3: {
-        "x_path": '//*[@id="hTContainer"]/div/div[1]/div[2]/nav/div/div/ul/li[5]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/ul/li[6]/div/div[1]/div[2]/div/div/div/ul/li[1]/a',
+        "x_path": '/html/body/div/header/div/div/div/div[2]/nav/div/div/ul/li[5]/div/div[2]/div/div/div/div[2]/div/div/div/ul/li[6]/div/div/div[2]/div/div/div/ul/li/button',
         "text": "Em posen una multa de trànsit",
     },
     4: {
-        "x_path": '//*[@id="hTContainer"]/div/div[1]/div[2]/nav/div/div/ul/li[5]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/ul/li[6]/div/div[1]/div[2]/div/div/div/ul/li[1]/div/div[1]/div[2]/div/div/div/ul/li[2]/p/a',
+        "x_path": '/html/body/div/header/div/div/div/div[2]/nav/div/div/ul/li[5]/div/div[2]/div/div/div/div[2]/div/div/div/ul/li[6]/div/div/div[2]/div/div/div/ul/li/div/div/div[2]/div/div/div/ul/li[2]/p/a',
         "text": 'Accedeix a "Em posen una multa de trànsit"',
     },
-    5: {"x_path": '//*[@id="c9c5d0b8-e18f-11ec-b15b-005056924a59"]/div[3]/div/div/div/p[1]/a', "text": "consultar-la i fer el pagament"},
-    6: {"x_path": '//*[@id="detall"]/div/div/div[4]/div[1]/div[2]/div/div[1]/div[1]/a', "text": "Inicia"},
+    5: {"x_path": '/html/body/main/section/article/div/div/div/div/div[3]/div/div/div/p/a', "text": "consultar-la i fer el pagament"},
+    6: {"x_path": '/html/body/main/section/article/div/div/div/div/div/div[4]/div/div[2]/div/div/div/a', "text": "Inicia"},
 }
 
 def get_step_by_path(x_path):
